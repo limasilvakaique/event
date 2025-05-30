8115008d5d9bc35fbb043f91366475d26bc69aec
gabriel <gabriel.silva1089@aluno.ce.gov.br>
2025-05-30 15:47:13 -0300

    .

---
+++ api-eventos/data.js
@@ -0,0 +1,9 @@
+let events = [
+    { id: 1, title: 'Encontro React', date: '2025-06-10' },
+    { id: 2, title: 'Workshop Node.js', date: '2025-06-20' }
+  ];
+  
+  let rsvps = [];
+  
+  module.exports = { events, rsvps };
+  
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,3 +6,30 @@ abstract class Controller
 {
     //
 }
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import EventListScreen from './screens/EventListScreen';
+import RsvpFormScreen from './screens/RsvpFormScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="EventList">
+        <Stack.Screen
+          name="EventList"
+          component={EventListScreen}
+          options={{ title: 'Lista de Eventos' }}
+        />
+        <Stack.Screen
+          name="RsvpForm"
+          component={RsvpFormScreen}
+          options={{ title: 'Confirmação de Presença' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
